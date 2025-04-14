34a6a4562d9e81718ca9635877c49530bdef3805
Abdulhakeem <abuabdillah5444@gmail.com>
2025-04-14 17:30:33 +0100

    edits

--- project-tf/main.tf
+++ project-tf/main.tf
@@ -31,11 +31,11 @@ module "vpc" {
   version = "5.19.0"
 
   name = "tikah-vpc"
-  cidr = "10.0.0.0/16"
+  cidr = "10.1.0.0/16"
 
   azs             = ["us-east-1a", "us-east-1b"]
-  private_subnets = ["10.0.1.0/24", "10.0.2.0/24"]
-  public_subnets  = ["10.0.101.0/24", "10.0.102.0/24"]
+  private_subnets = ["10.1.1.0/24", "10.1.2.0/24"]
+  public_subnets  = ["10.1.101.0/24", "10.1.102.0/24"]
 
   enable_nat_gateway = true
   single_nat_gateway = true
